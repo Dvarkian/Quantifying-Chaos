f6165cebe9e6f49a1ee6951e3ee0d6df7e36df29
Mblizzard <47677744+Mblizzard@users.noreply.github.com>
2022-10-04 09:39:42 +1100

    Add basic UI frame.

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 import PySimpleGUI as sg
+
+
+canvas_size = (600, 400) # [px]
+
+coord_size = (100, 100) # [arb.]
+
+
+layout = [[sg.Graph(canvas_size, (0, 0), coord_size,
+                    background_color = "grey5")]]
+
+
+window = sg.Window("Quantifying Chaos", layout,
+                   background_color="grey15",
+                   finalize=True,
+                   margins=(10, 10))
+
+
+while True:
+
+    event, values = window.read(timeout=10)
+
+    if event == sg.WIN_CLOSED:
+        window.close()
+        break;
+
+    
