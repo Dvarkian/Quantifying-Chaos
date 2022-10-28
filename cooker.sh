d323400c37f51cf8ae459d2d110c102aacb7ecd8
rjon6734 <rjon6734@uni.sydney.edu.au>
2022-10-28 15:00:43 +1100

    Fixed the weight issue

--- cooker.sh
+++ cooker.sh
@@ -5,21 +5,21 @@ mkdir "cooked_data"
 
 mkdir "cooked_data/angles"
 mkdir "cooked_data/angles_polar"
-for file in data/angles/*.csv
+for file in raw_data/angles/*.csv
 do
     python3 cooking/cooker.py $file  cooked_data/angles/ cooked_data/angles_polar/
 done
 
 mkdir "cooked_data/propogation"
 mkdir "cooked_data/propogation_polar"
-for file in data/propogation/*.csv
+for file in raw_data/propogation/*.csv
 do
     python3 cooking/cooker.py $file  cooked_data/propogation/ cooked_data/propogation_polar/
 done
 
 mkdir "cooked_data/weights"
 mkdir "cooked_data/weights_polar"
-for file in data/angles/*.csv
+for file in raw_data/weights/*.csv
 do
     python3 cooking/cooker.py $file  cooked_data/weights/ cooked_data/weights_polar/
 done
